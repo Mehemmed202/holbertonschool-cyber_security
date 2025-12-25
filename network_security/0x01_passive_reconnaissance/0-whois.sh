4b50abae056184cd9e459b6561cbcb0ea40827df
Mehemmed202 <m3mm3dov857@gmail.com>
2025-12-25 16:59:24 -0500

    m

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': *' '/Registrant|Admin|Tech/ {print $1 ", " $2}' | awk '{gsub(",", ""); print $0}' >> "$1.csv"
+whois "$1" | awk -F': *' '/Registrant|Admin|Tech/ {print $1 ", " $2}' | awk  'NR==1 {gsub(",", "", $0)} {print}' >> "$1.csv"
