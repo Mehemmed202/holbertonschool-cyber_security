82cd1aeae25afd036bfa2be9b7faaf3e651446ea
Mehemmed202 <m3mm3dov857@gmail.com>
2026-04-09 17:56:24 +0400

    m

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-awk '{print $12}' logs.txt | sort | uniq -c | sort -rn | head -n 1 | awk '{print $2}' 
+awk -F\" '{print $6}' logs.txt | sort | uniq -c | sort -rn | head -n 1 | awk '{print $2}' 
