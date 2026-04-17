069f92341e17c147392e6d463bf7888bb7ced082
Mehemmed202 <m3mm3dov857@gmail.com>
2026-04-17 15:18:11 +0400

    Users Account

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '{print $8}' auth.log | grep "name=" | awk -F "=" '{print $2}' | sort | uniq | tr -d "\n" | head -c -1
+awk '{print $8}' auth.log | grep "name=" | awk -F "=" '{print $2}' | sort | uniq | tr -d "\n" | paste -sd "," -
