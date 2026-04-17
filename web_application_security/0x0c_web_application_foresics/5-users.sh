ad70e32503f97c0044604152c2c41ef566ef30ae
Mehemmed202 <m3mm3dov857@gmail.com>
2026-04-17 15:30:46 +0400

    Users Account

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '{print $8}' auth.log | grep "name=" | awk -F "=" '{print $2}' | sort | uniq | tr "\n" "," | sed 's/,S/\n/'
+grep "useradd" auth.log | grep -o 'name=[^ ]*' | awk -F'[=,]' '{print $2}' | sort | uniq | tr '\n' ',' | sed 's/,$/\n/'
