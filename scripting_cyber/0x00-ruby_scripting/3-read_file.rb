53b637ea67e19c314fbdbab4067b0f1f8f1881c8
Mehemmed202 <m3mm3dov857@gmail.com>
2026-05-24 17:53:29 +0400

    3-read_file.rb

--- scripting_cyber/0x00-ruby_scripting/3-read_file.rb
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -1,15 +1,19 @@
 require 'json'
-def count_user_ids(path)
-  file_content=File.read(path)
-  data=JSON.parse(file_content)
-  id_num=hash.new(0)
 
-  data.each do |row|
-    user_id=row['userId']
-    id_num[user_id] += 1 if user_id
+def count_user_ids(path)
+  # JSON faylını oxu
+  data = JSON.parse(File.read(path))
+
+  # userId saylarını saxlamaq üçün hash
+  counts = Hash.new(0)
+
+  # Hər element üçün userId-ni say
+  data.each do |item|
+    counts[item["userId"]] += 1
   end
 
-  id_num.each do |user_id, num|
-    puts"#{user_id}: #{say}"
+  # Nəticəni çap et
+  counts.each do |user_id, count|
+    puts "#{user_id}: #{count}"
   end
 end
