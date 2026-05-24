86509f73afdc397ee653c706eabbc5b60a83b965
Mehemmed202 <m3mm3dov857@gmail.com>
2026-05-24 17:54:05 +0400

    3-read_file.rb

--- scripting_cyber/0x00-ruby_scripting/3-read_file.rb
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -1,18 +1,14 @@
 require 'json'
 
 def count_user_ids(path)
-  # JSON faylını oxu
   data = JSON.parse(File.read(path))
 
-  # userId saylarını saxlamaq üçün hash
   counts = Hash.new(0)
 
-  # Hər element üçün userId-ni say
   data.each do |item|
     counts[item["userId"]] += 1
   end
 
-  # Nəticəni çap et
   counts.each do |user_id, count|
     puts "#{user_id}: #{count}"
   end
