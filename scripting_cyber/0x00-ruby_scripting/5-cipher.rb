05a46afeb1ac596c0c6c5ded3cfd4b326ee8a580
Mehemmed202 <m3mm3dov857@gmail.com>
2026-05-24 18:57:40 +0400

    5-cipher.rb

---
+++ scripting_cyber/0x00-ruby_scripting/5-cipher.rb
@@ -0,0 +1,25 @@
+class CaesarCipher
+  def initialize(shift)
+    @shift=shift
+  end
+  
+  def encrypt(message)
+    cipher(message, @shift)
+  end
+  
+  def decrypt(message)
+    cipher(message, -@shift)
+  end
+
+  def cipher(message, shift)
+    message.chars.map do |char|
+      if char.match?(/[A-Z]/)
+        (((char.ord - 65 + shift) % 26) +65).chr
+      elsif char.match?(/a-z/)
+        (((char.ord - 97 + shift) % 26) +65).chr
+      else
+        char
+      end
+    end.join
+  end
+end
