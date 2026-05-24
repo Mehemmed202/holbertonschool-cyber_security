b6136ccc3eee4f2e58e5ee2f15481c9d6bea3512
Mehemmed202 <m3mm3dov857@gmail.com>
2026-05-24 19:00:55 +0400

    5-cipher.rb

--- scripting_cyber/0x00-ruby_scripting/5-cipher.rb
+++ scripting_cyber/0x00-ruby_scripting/5-cipher.rb
@@ -15,7 +15,7 @@ class CaesarCipher
     message.chars.map do |char|
       if char.match?(/[A-Z]/)
         (((char.ord - 65 + shift) % 26) +65).chr
-      elsif char.match?([/a-z/])
+      elsif char.match?(/[a-z]/)
         (((char.ord - 97 + shift) % 26) +97).chr
       else
         char
