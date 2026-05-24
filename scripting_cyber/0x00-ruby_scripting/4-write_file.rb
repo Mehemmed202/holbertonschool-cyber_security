5f8d43f719fe4a73ee73be1bfe4f60b103db01e5
Mehemmed202 <m3mm3dov857@gmail.com>
2026-05-24 18:24:38 +0400

    4-write_file.rb

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,6 +1,8 @@
 require 'json'
 
 def merge_json_files(file1_path, file2_path)
+  puts "Merged JSON written to #{file2_path}"
+  puts "File merged successfully!"
   data1 = JSON.parse(File.read(file1_path))
   data2 = JSON.parse(File.read(file2_path))
 
